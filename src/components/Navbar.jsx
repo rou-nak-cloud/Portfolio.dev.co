b970e1706ecb57f145a2be5b10984c973e282726
rou-nak-cloud <github.rounak.19@gmail.com>
2025-08-26 10:36:47 +0530

    navbar with animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import { navLogos } from '../assets/assets'
+import { motion } from 'motion/react'
 
 const Navbar = () => {
+    const variants = {
+        hidden:{opacity:0,y:-10},
+        visible:(idx) => ({opacity:1, y:0,transition:{delay: idx * 0.4,duration:0.8, ease:"easeOut"}}),
+    }
   return (
     <>
       <div className="navbar h-14 px-12 sm:px-16 md:px-10 lg:px-36 xl:px-50">
         {/* sidebar */}
         <div className="wrapper max-w-[1466px] h-full m-auto flex items-center justify-between">
-          <h3 className="font-bold text-base sm:text-sm md:text-sm lg:text-md cursor-grab select-none">
-            Rounak <span className="text-[#FF4040] text-lg">Bakshi</span>
-          </h3>
+          <motion.h3 
+            initial={{opacity:0,scale:0.3}}
+            animate={{opacity:1,scale:1}}
+            transition={{duration:1}}
+          className="font-bold text-base sm:text-sm md:text-sm lg:text-md cursor-grab select-none">
+            Rounak <span className="text-[#FF4040] text-lg">Bakshi.</span>
+          </motion.h3>
 
           <div className="social flex items-center gap-2 sm:gap-3 md:gap-4">
             {navLogos.map((logo, idx) => (
-              <li key={idx} className="list-none">
+              <motion.li key={idx} className="list-none" 
+              variants={variants}
+              initial='hidden'
+              animate='visible'
+              custom={idx}
+              >
                 <a href={logo.link}>
                   <img
                     src={logo.icon}
@@ -22,7 +36,7 @@ const Navbar = () => {
                                hover:scale-125 transition-transform duration-300"
                   />
                 </a>
-              </li>
+              </motion.li>
             ))}
           </div>
         </div>
