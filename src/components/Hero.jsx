fb4c2cc2ca9cbdcf11e3193b51668815d2247f2b
rou-nak-cloud <github.rounak.19@gmail.com>
2025-08-27 18:46:32 +0530

    hero animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,36 +1,81 @@
 import React from 'react'
 import assets from '../assets/assets'
+import { motion } from 'motion/react'
+
+const textVariants = {
+    initial:{
+        x:-500,
+        opacity:0,
+    },
+    animate:{
+        x:0,
+        opacity:1,
+        transition:{
+            duration:1,
+            stagerChildren:0.2,
+        },
+    },
+    scrollPng:{
+        opacity:0,
+        y:9,
+        transition:{
+            duration:4,
+            repeat:Infinity,
+        },
+    },
+}
+const bigTextVariants = {
+    initial:{
+        x:0,
+    },
+    animate:{
+        x:"-250%",
+        transition:{
+            repeat:Infinity,
+            repeatType:"mirror",
+            duration:20,
+        },
+    },
+}
 
 const Hero = () => {
   return (
     <>
      <div className='hero overflow-hidden relative min-h-[calc(100vh-40px)] sm:min-h-[calc   (100vh-60px)] md:min-h-[calc(100vh-70px)] w-full'>
         <div className='wrapper w-[1000px] h-screen flex items-center justify-center'>
-            <div className='textContainer h-screen w-[600px] flex flex-col justify-center gap-9 z-[9]'>
-                <h2 className='text-3xl tracking-[9px] font-semibold bg-gradient-to-r from-red-500 to-purple-600 bg-clip-text text-transparent'>ROUNAK BAKSHI</h2>
-                <h1 className='text-7xl'>Web developer <span className='text-purple-800/90 cursor-grab'>&</span> Electronics engineer<span className='text-red-500'>.</span></h1>
-                <div className="buttons flex gap-4 z-[10]">
+            <motion.div
+            variants={textVariants}
+            initial='initial'
+            animate='animate'
+            className='textContainer h-screen w-[600px] flex flex-col justify-center gap-9 z-[9]'>
+                <motion.h2 variants={textVariants} className='text-3xl tracking-[9px] font-semibold bg-gradient-to-r from-red-500 to-purple-600 bg-clip-text text-transparent'>ROUNAK BAKSHI</motion.h2>
+                <motion.h1 variants={textVariants} className='text-7xl'>Web developer <span className='text-purple-800/90 cursor-grab'>&</span> Electronics engineer<span variants={textVariants} className='text-red-500'>.</span></motion.h1>
+                <motion.div variants={textVariants} className="buttons flex gap-4 z-[10]">
                       <a 
                             href="https://github.com/rou-nak-cloud" 
                             target="_blank" 
                             rel="noopener noreferrer"
                         >
-                    <button className="p-4 border border-purple-800 rounded-lg font-light cursor-pointer transition-all duration-500 ease-in-out hover:bg-gradient-to-r hover:from-red-600 hover:to-purple-700 
+                    <motion.button variants={textVariants} className="p-4 border border-purple-800 rounded-lg font-light cursor-pointer transition-all duration-500 ease-in-out hover:bg-gradient-to-r hover:from-red-600 hover:to-purple-700 
                        hover:text-white hover:scale-105">
-                        See the projects in <span className='font-bold text-white/90'>github</span>
-                    </button>
+                        See the projects in <motion.span className='font-bold text-white/90'>github</motion.span>
+                    </motion.button>
                     </a>
-                    <button className="p-4 border border-purple-800 rounded-lg font-light cursor-pointer transition-all duration-300 ease-in-out  hover:bg-gradient-to-r hover:from-purple-700 hover:to-red-600 
+                    <motion.button variants={textVariants} className="p-4 border border-purple-800 rounded-lg font-light cursor-pointer transition-all duration-300 ease-in-out  hover:bg-gradient-to-r hover:from-purple-700 hover:to-red-600 
                        hover:text-white hover:scale-105">
                         Know about <span className='font-bold text-white/90'>me</span>
-                    </button>
-                </div>
-                <img src={assets.scroll} alt="scroll" className='w-12 cursor-all-scroll z-[10] opacity-75' />
-            </div>
+                    </motion.button>
+                </motion.div>
+                <motion.img src={assets.scroll} alt="scroll" 
+                variants={textVariants} animate="scrollPng"
+                className='w-12 cursor-all-scroll z-[10] opacity-60'/>
+            </motion.div>
         </div>
-        <div className='slidingText absolute text-[50vh] bottom-[-80px] whitespace-nowrap opacity-10 select-none'>
+        <motion.div
+        variants={bigTextVariants} initial="initial" animate="animate"
+        className='slidingText absolute w-1/2 font-semibold text-[50vh] bottom-[-80px] whitespace-nowrap opacity-10 select-none'>
             Full Stack Coder <span className=''>&</span> Calisthenic Athlete
-        </div>
+        </motion.div>
             {/* animation gpt */}
            <div className="image absolute h-full w-1/2 top-0 right-0 flex items-center justify-center overflow-hidden">
                 <div className="relative w-[400px] h-[400px]">
